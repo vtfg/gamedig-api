8cca996ab018e1f4c8ebcc6c027df8661bf224e5
Victor Guedes <victorguedes@localhost.local>
2020-08-23 00:45:15 -0300

    :sparkles: Handling erros

--- app.js
+++ app.js
@@ -6,4 +6,20 @@ const info = require("./routes/info");
 app.use(morgan("dev"));
 app.use("/info", info);
 
+app.use((req, res, next) => {
+    const error = new Error("There's an error");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+
+    return res.send({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
 module.exports = app;
